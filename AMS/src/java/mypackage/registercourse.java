d89d1acb80c76f089ff0e9ed19236174d93ef4e8
User <User@LAPTOP-7TSVJB2J>
2017-12-17 07:50:04 +0800

    Merge origin/master
    Conflicts:
    	AMS/web/index.html

---
+++ AMS/src/java/mypackage/registercourse.java
@@ -0,0 +1,69 @@
+package mypackage;
+
+import java.sql.*;
+import java.io.*;
+import static java.lang.System.out;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import javax.servlet.annotation.WebServlet;
+import database_connection.connect_mysql;
+
+@WebServlet(name="registercourse",urlPatterns=("/registercourse"))
+
+public class registercourse extends HttpServlet {
+  public void doPost(HttpServletRequest request,
+                     HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+      storeUser(request);
+      showConfirmation(request, out);
+    
+  }
+
+  
+
+
+ 
+  /** Show a confirmation page when they press the
+   *  "Submit" button.
+   */
+  
+  private void showConfirmation(HttpServletRequest request,
+                                PrintWriter out) {
+                out.println("<script type=\'text/javascript\'>");
+                out.println("alert('Register Course Succesful');");
+                out.println("location='./student/registercourse.jsp'");
+                out.println("</script>");
+  }
+
+  /** Why it is bad to give your email address to
+   *  untrusted sites.
+   */
+  
+  private void storeUser(HttpServletRequest request) {
+    String studentid = request.getParameter("studentid");
+    String subject = request.getParameter("subject");
+    String section = request.getParameter("section");
+    String coursename = request.getParameter("coursename");
+
+      
+      try{
+            Connection con = connect_mysql.getSQLServerConnection();
+            Statement st=con.createStatement();
+            
+            
+            st.executeUpdate("INSERT INTO studying(studentid,subject,section,coursename) VALUES ('"+studentid+"','"+subject+"','"+section+"','"+coursename+"');");
+      
+            con.close();
+     
+                 }
+            catch(Exception e){
+                out.println(e);
+            }
+            
+  }
+  
+ 
+}
